7b781bea868c7ca50eb8c774df7847ab98a87f19
josh-codi <odamejoshua37@gmail.com>
2023-07-06 06:06:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './Pages/HomePage/HomePage';
+import About from './Pages/About/About'
+import Awards from './Pages/AwardsPage/Awards'
+import Award from './Pages/Award/Award'
+import Header from './Common/Header/Header';
+import Footer from './Common/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container border h-screen">
+      <Header/>
+      <div className={`w-full border`} style={{height: 'calc(100% - 65px)'}}>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/awards' element={<Awards/>} />
+          <Route path='/award:id' element={<Award/>} />
+
+        </Routes>
+        <Footer/>
+      </div>
     </div>
   );
 }
---
+++ src/Common/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='w-full bg-[#0047938e]' style={{minHeight: '65px'}}>
+
+    </header>
+  )
+}
